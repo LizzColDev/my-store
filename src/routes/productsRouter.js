34ee98d285074d1f335793e96ce5845ea711f374
LizzColDev <lquintero28@live.com>
2023-07-24 18:20:36 -0500

    Added services and routes

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,27 +1,56 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
-const app = express();
+const ProductsService = require('../services/productsService');
 
 const router = express.Router();
+const service = new ProductsService();
 
-app.get('/', (req, res) =>{
-  const products = [];
-  const {size} = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      Image: faker.image.url(),
-    });
-  }
-
+router.get('/', (req, res) =>{
+  const products = service.find();
   res.json(products);
 });
 
 // app.get('/filter', (req, res) => {
 
 // });
+
+router.get('/:id', (req, res) => {
+
+  const { id } = req.params;
+  const product = service.findOne(id);
+
+  if(!product){
+    res.status(404).json({
+      message: 'Product not found'
+    })
+  }
+  res.json(product);
+})
+
+router.post('/', (req, res)=>{
+  const body = req.body;
+  res.status(201).json({
+    message: 'created',
+    data: body
+  })
+})
+
+router.patch('/:id', (req, res)=>{
+  const {id} = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id
+  })
+})
+
+router.delete('/:id', (req, res)=>{
+  const {id} = req.params;
+  res.json({
+    message: 'deleted',
+    id
+  })
+});
 
 module.exports = router;
 
